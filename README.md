6f0cc480d9b05a19437828f4daedb6cb37231ce2
Nikddimos Gebregiorgis <gebrenick07@gmail.com>
2019-12-12 20:32:26 -0700

    updating homework

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Homework-3
+** password generator 
+
+// Generator random password
+// set password length/ complexity
+//possible password values
+//create for loop to choose password characters
+//add password to textbox/display area
--- scripts.js
+++ scripts.js
@@ -1,8 +1,17 @@
 // Generator random password
 
-function generate(){
+function generate() {
     // set password length/ complexity
-    let complexity = document.getElementById("slider").Value;
-    
-    let Value = "ABCDEFGHIJKLMNOPQ"
+    var complexity = document.getElementById("slider").Value;
+
+    //possible password values 
+    var Value = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmopqurstuvwxyz1234567890!@#$%&*^()_+";
+    var password = "";
+
+    //create for loop to choose password characters
+    for (var i = 0; i <= complexity; i++) {
+        password = password + Value.charAt(Math.floor(Math.random() * Math.floor(Value.length - 1)));
+    }
+    //add password to textbook/display area 
+    document.getElementById("display").Value = password
 }
